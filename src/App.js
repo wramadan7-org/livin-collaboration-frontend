af8f354e3657b7550a0fdaa8c2b77de9f596ea94
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-12 16:10:16 +0700

    delete comment

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import {
 import LivinWebsite from './pages/LivinWebsite';
 import LoginPage from './pages/LoginPage';
 
-// import TodoApp from './pages/TodoApp';
-
 function App() {
   return (
     <Router>
-      {/* <LivinWebsite /> */}
       <Routes>
         <Route path="/" element={<LivinWebsite />} />
         <Route path="/login" element={<LoginPage />} />
