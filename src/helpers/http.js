d79156343bf36462b2c5005fc03f31a8664d275a
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-12 19:55:14 +0700

    use static baseURL

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line import/no-named-default
 import { default as axios } from 'axios';
 
-const { BASE_URL } = process.env;
+// const { BASE_URL } = process.env;
 
 const http = (token = false) => axios.create({
-  baseURL: BASE_URL,
+  // baseURL: BASE_URL,
+  baseURL: 'http://localhost:8080/v1',
   headers: {
     Authorization: token ? `Bearer ${token}` : undefined,
   },
