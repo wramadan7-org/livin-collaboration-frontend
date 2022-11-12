ce5ee0e55d16678113a3bf2406e63adc50cb0793
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-12 16:33:52 +0700

    Storing redux

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Routes,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import LivinWebsite from './pages/LivinWebsite';
 import LoginPage from './pages/LoginPage';
 
+// Import Store
+import store from './reduxs/store';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LivinWebsite />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <Provider store={store().store}>
+      <PersistGate loading={null} persistor={store().persistor}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LivinWebsite />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
